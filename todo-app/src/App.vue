74cc542e232cf91939d858c5d5cf80d26666d2be
snehal <snehal.m.0087@gmail.com>
2021-11-04 15:18:20 +0530

    component added

---
+++ todo-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<h1>ToDo App</h1>
+<Container />
+</template>
+
+<script>
+import Container from './components/Container.vue';
+
+export default {
+  name: 'App',
+  components: {
+  Container
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todo-app/src/components/Container.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container">
+        <input type="text" />
+        <button>Create</button>
+    </div>
+</template>
